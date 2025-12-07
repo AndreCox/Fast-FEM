9c747ad2bb95e995b1e745c214620610bed10295
Andre Cox <arc.xoc@gmail.com>
2025-12-06 22:27:48 -0800

    Windows version fix uint8 and build version change

--- src/serialization.cpp
+++ src/serialization.cpp
@@ -4,7 +4,7 @@
 // Writes a std::string to an ofstream, prefixing it with its length (uint32_t)
 void writeString(std::ofstream &ofs, const std::string &str)
 {
-    u_int32_t len = static_cast<u_int32_t>(str.length());
+    std::uint32_t len = static_cast<std::uint32_t>(str.length());
     ofs.write((char *)&len, sizeof(len));
     if (len > 0)
     {
@@ -15,7 +15,7 @@ void writeString(std::ofstream &ofs, const std::string &str)
 // Reads a std::string from an ifstream
 std::string readString(std::ifstream &ifs)
 {
-    u_int32_t len = 0;
+    std::uint32_t len = 0;
     ifs.read((char *)&len, sizeof(len));
     if (len > 0)
     {
