b69cf1965efe56a264771773104609667bfdda60
AndreCox <arc.xoc@gmail.com>
2025-12-06 18:31:40 -0800

    Debugging Changes

--- src/main.cpp
+++ src/main.cpp
@@ -45,6 +45,17 @@ int main()
     const double E_steel = 30e6;                                            // Psi
     const double E_aluminum = 10e6;                                         // Psi
 
+    // idiot check print out material and beam properties
+    std::cout << "Steel Material Properties:" << std::endl;
+    std::cout << "  Young's Modulus: " << E_steel << " Psi" << std::endl;
+    std::cout << "  Area: " << A_steel << " in^2" << std::endl;
+    std::cout << "  Moment of Inertia: " << I_steel << " in^4" << std::endl;
+
+    std::cout << "Aluminum Material Properties:" << std::endl;
+    std::cout << "  Young's Modulus: " << E_aluminum << " Psi" << std::endl;
+    std::cout << "  Area: " << A_aluminum << " in^2" << std::endl;
+    std::cout << "  Moment of Inertia: " << I_aluminum << " in^4" << std::endl;
+
     MaterialProfile steel_material = {"Steel", E_steel};
     MaterialProfile aluminum_material = {"Aluminum", E_aluminum};
     BeamProfile steel_beam = {"Steel Beam", A_steel, I_steel, S_steel};
@@ -66,31 +77,43 @@ int main()
         aluminum_truss,
     };
 
-    // Define nodes
     std::vector<Node> nodes = {
-        Node(12.0f, 0.0f, Free),
-        Node(12.0f, 6.0f, Free),
-        Node(0.0f, 0.0f, Slider, 90.0f),
-        Node(0.0f, 10.0f, Free),
-        Node(-10.0f, 10.0f, Fixed),
+        Node(0.0f, 0.0f, Fixed),
+        Node(10.0f, 0.0f, Free),
     };
 
-    // Define beams (beams)
-    std::vector<Beam>
-        beams = {
-            Beam(0, 1, 0, 2),
-            Beam(0, 2, 1, 3),
-            Beam(1, 2, 0, 2),
-            Beam(1, 3, 1, 3),
-            Beam(2, 3, 0, 2),
-            Beam(3, 4, 1, 1),
-        };
+    std::vector<Beam> beams = {
+        Beam(0, 1, 1, 1),
+    };
+
+    // Define nodes
+    // std::vector<Node> nodes = {
+    //     Node(12.0f, 0.0f, Free),
+    //     Node(12.0f, 6.0f, Free),
+    //     Node(0.0f, 0.0f, Slider, 90.0f),
+    //     Node(0.0f, 10.0f, Free),
+    //     Node(-10.0f, 10.0f, Fixed),
+    // };
+
+    // // Define beams (beams)
+    // std::vector<Beam>
+    //     beams = {
+    //         Beam(0, 1, 0, 2),
+    //         Beam(0, 2, 1, 3),
+    //         Beam(1, 2, 0, 2),
+    //         Beam(1, 3, 1, 3),
+    //         Beam(2, 3, 0, 2),
+    //         Beam(3, 4, 1, 1),
+    //     };
 
     FEMSystem fem_system(nodes, beams, material_profiles, beam_profiles);
 
+    fem_system.unit_system = Imperial;
+    fem_system.forces(1 * 3 + 1) = -10.0; // Apply a downward force of 10 lbs at node 0
+
     // Course Project 2 Loads
-    fem_system.forces(0 * 3) = -1000.0;
-    fem_system.forces(0 * 3 + 1) = -1732.0;
+    // fem_system.forces(0 * 3) = -1000.0;
+    // fem_system.forces(0 * 3 + 1) = -1732.0;
 
     // Initialize the renderer
     GraphicsRenderer renderer(fem_system);
